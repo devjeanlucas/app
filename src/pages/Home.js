1f3df988d73179941776c7b511fe2b7bc61e6396
Jean lucas <Lucas__jean@outlook.com>
2023-02-09 10:42:38 -0300

    t

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,13 @@
-import SecondSection from "../layouts/secondSection"
+
 import SectionExperience from "../layouts/sectionExperience"
-import SectionText from "../layouts/SectionText"
+
 import SectionMainHome from "../layouts/SectionMainHome"
 
 export default function Expo () {
     return (
         <div>
             <SectionMainHome/>
-            <SecondSection/>
             <SectionExperience/>
-            <SectionText/>
         </div>
     )
 }
