b10895ab5dece844ab362b68889087887afb892a
Jean lucas <Lucas__jean@outlook.com>
2023-02-14 16:44:20 -0300

    consertando id inicial

--- src/components/TelaPagamentoMobile.js
+++ src/components/TelaPagamentoMobile.js
@@ -84,7 +84,6 @@ export default function CheckOut() {
                     <div className={styles.cartao_info}><p>em até <span>{parcelamento[1]}x</span> de <span>R$ {parcelamento[0]}</span> sem Juros</p></div>
                 </div>
                 <Link to="/carrinho">ver carrinho</Link>
-                
                 <Link to="/checkOut/usuario">
                     <button className={styles.btn_checkout}
                     >Finalizar</button>
